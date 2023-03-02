6ec981943cb128ecce7251e1db352a6aca740184
BARALLL <54141221+BARALLL@users.noreply.github.com>
2023-03-02 10:06:05 +0100

    node

--- HTTP-Server/api.c
+++ HTTP-Server/api.c
@@ -1,7 +1,104 @@
 #include "api.h"
 
+#include "api.h"
+#include <stdio.h> 
 
-void* getRootTree()
+
+
+void addElement(_Token** head, void* node) {
+    _Token* newToken = (_Token*)malloc(sizeof(_Token));
+    newToken->node = node;
+    newToken->next = NULL;
+
+    if (*head == NULL)
+        *head = newToken;
+    else {
+        _Token* currToken = *head;
+        while (currToken->next != NULL)
+            currToken = currToken->next;
+
+        currToken->next = newToken;
+    }
+}
+
+long removeElement(long* param_1)
 {
+    long lVar1;
+    if (param_1 == (long*)0x0) {
+        lVar1 = 0;
+    }
+    else {
+        lVar1 = *param_1;
+        if (lVar1 != 0) {
+            *param_1 = *(long*)(lVar1 + 8);
+            return lVar1;
+        }
+    }
+    return lVar1;
+}
 
+
+_Token* searchTree(void* start, char* name) {
+    _Token* list = (_Token*)malloc(sizeof(_Token));
+    list->node = NULL;
+    list->next = NULL;
+
+    derivation_tree* start_node = NULL;
+    if (start == NULL) start_node = root;
+    else start_node = (derivation_tree*)start;
+
+    if (strcmp(start_node->tag, name) == 0 && start_node->value_length != NOT_VALID) list->node = start_node;
+    else recursif_search_tree(start_node, name, list);
+    if (list->node == NULL && list->next == NULL)
+    {
+        free(list);
+        //printf("Didn't Find any %s !\n", name);
+        return NULL;
+    }
+    return list;
+}
+
+
+char* getElementTag(void* node, int* len)
+{
+    if (node == NULL)
+        return NULL;
+
+    char* str = tree_node_string[((tree_node*)node)->type];
+    *len = strlen(str);
+    return str;
+}
+
+
+char* getElementValue(void* node, int* len)
+{
+    if (node == NULL)
+        return NULL;
+
+    tree_node* tnode = (tree_node*)node;
+    char* str = copy_sub_str(tnode->string, tnode->start_string, tnode->length_string);
+    *len = strlen(str);
+    return str;
+}
+
+
+void purgeElement(_Token** root) {
+    
+    _Token* currToken = *root;
+
+    while (currToken != NULL) 
+    {
+        _Token* tmp = currToken;
+        currToken = currToken->next;
+        free(tmp);
+    }
+    *root = NULL;
+}
+
+
+void purgeTree(void* root) 
+{
+    if (root == NULL) return;
+    tree_node_free(root);
+    root = NULL;
 }
--- HTTP-Server/api.h
+++ HTTP-Server/api.h
@@ -5,8 +5,17 @@ typedef struct _token {
 		struct _token *next; 	// next pointe vers le prochain token.
 		} _Token;
 
+typedef struct _treeNode
+{
+	void* nodeData;
+	struct _treeNode* parent;
+	struct _treeNode* nextSib;
+	struct _treeNode* prevSib;
+	struct _treeNode* child;
+} treeNode;
+
 // Fonction qui retourne un pointeur (type opaque) vers la racine de l'arbre construit. 
-void *getRootTree(); 
+void *getRootTree();
 
 // Fonction qui recherche dans l'arbre tous les noeuds dont l'etiquette est egale à la chaine de caractères en argument.   
 // Par convention si start == NULL alors on commence à la racine 
@@ -15,17 +24,17 @@ _Token *searchTree(void *start,char *name);
 
 // fonction qui renvoie un pointeur vers char indiquant l'etiquette du noeud. (le nom de la rulename, intermediaire ou terminal) 
 // et indique (si len!=NULL) dans *len la longueur de cette chaine.
-char *getElementTag(void *node,int *len); 
+char *getElementTag(void *node,int *len);
 
 // fonction qui renvoie un pointeur vers char indiquant la valeur du noeud. (la partie correspondnant à la rulename dans la requete HTTP ) 
 // et indique (si len!=NULL) dans *len la longueur de cette chaine.
-char *getElementValue(void *node,int *len); 
+char *getElementValue(void *node,int *len);
 
 // Fonction qui supprime et libere la liste chainée de reponse. 
-void purgeElement(_Token **r); 
+void purgeElement(_Token **r);
 
 // Fonction qui supprime et libere toute la mémoire associée à l'arbre. 
-void purgeTree(void *root); 
+void purgeTree(void *root);
 
 // L'appel à votre parser un char* et une longueur à parser.  
-int parseur(char *req, int len); 
+int parseur(char *req, int len);
